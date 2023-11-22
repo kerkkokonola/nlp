1e18d8ddd2fcf815e4104ef4a3638becfa8b5afc
Ilia <ilia.zalesskii@protonmail.com>
2023-11-22 22:37:08 +0200

    some formatting

--- preprocess.py
+++ preprocess.py
@@ -7,7 +7,7 @@
 # If not, you can install them with pip via command "pip3 install scikit-learn nltk"
 # Load the required packages that are used in this example.
 
-#Edit / Kerkko: added nltk downloads to run script. Our text list is now the list of titles and abstracts.
+# Edit / Kerkko: added nltk downloads to run script. Our text list is now the list of titles and abstracts.
 import re
 from nltk.tokenize import word_tokenize
 from nltk.stem import PorterStemmer
@@ -19,6 +19,7 @@ from string import punctuation
 import numpy as np
 
 import nltk
+
 nltk.download('punkt')
 nltk.download('stopwords')
 
@@ -29,7 +30,7 @@ reader = open(data_path, 'r', encoding='utf-8')
 lines = reader.readlines()
 
 # Extract the text (title + abstract) from each line
-text = [i.split('#')[1] + i.split('#')[2] for i in lines ] 
+text = [i.split('#')[1] + ' ' + i.split('#')[2] for i in lines[1:]]
 
 # some examples
 print('First documents:')
@@ -37,15 +38,14 @@ for i in text[:10]:
     print(i)
 print()
 
-
 # Preprocessing
 
 # Step 1: tokenization and lowercasing
 tokens_list = [word_tokenize(i) for i in text]
 
-lc_tokens_list = []    
-for i in tokens_list: 
-    lc_tokens_list.append([token.lower() for token in i]) 
+lc_tokens_list = []
+for i in tokens_list:
+    lc_tokens_list.append([token.lower() for token in i])
 
 print('After tokenization and lowercasing:')
 for i in lc_tokens_list[:10]:
@@ -56,8 +56,6 @@ print()
 uniques = np.unique([tok for doc in lc_tokens_list for tok in doc])
 print("Original number of tokens: {}\n".format(len(uniques)))
 
-
-
 # Steps 2 and 3: remove stop words and punctuation
 stop_words = set(stopwords.words('english'))
 print('NLTK stopwords:')
@@ -65,40 +63,38 @@ print(stop_words)
 print()
 
 # Here we include the punctuation in the stop words set. There are alternative
-#ways to remove punctuation.
+# ways to remove punctuation.
 stop_words.update(punctuation)
 stop_words.add("...")
 
-#you can check updated stopwords
-#print(stop_words)
+# you can check updated stopwords
+# print(stop_words)
 
-filtered_sentence = []    
-for i in lc_tokens_list: 
-    filtered_sentence.append([token for token in i if token not in stop_words]) 
-    
+filtered_sentence = []
+for i in lc_tokens_list:
+    filtered_sentence.append([token for token in i if token not in stop_words])
+
 # Numbers are also removed
-filtered_sentence = [ ' '.join(i) for i in filtered_sentence ]
-filtered_sentence = [ re.sub(r'\d+', '', sentence) for sentence in filtered_sentence ]
+filtered_sentence = [' '.join(i) for i in filtered_sentence]
+filtered_sentence = [re.sub(r'\d+', '', sentence) for sentence in filtered_sentence]
 
 # number of tokens
 uniques = np.unique([tok for doc in filtered_sentence for tok in doc.split()])
 print("Number of tokens after stopword and punctuation removal: {}\n".format(len(uniques)))
-
 
 print('After removing stop words, punctuation and numbers:')
 for i in filtered_sentence[:10]:
     print(i)
 print()
 
-
 # Step 4: stemming
 porter = PorterStemmer()
-#or snowball stemmer
-#stemmer = SnowballStemmer("english",ignore_stopwords=True)
+# or snowball stemmer
+# stemmer = SnowballStemmer("english",ignore_stopwords=True)
 stemmed_tokens_list = []
 
 for i in filtered_sentence:
-	stemmed_tokens_list.append([porter.stem(j) for j in i.split()])
+    stemmed_tokens_list.append([porter.stem(j) for j in i.split()])
 
 # number of tokens
 uniques = np.unique([tok for doc in stemmed_tokens_list for tok in doc])
@@ -106,34 +102,31 @@ print("Number of tokens after stemming: {}\n".format(len(uniques)))
 
 print('After stemming:')
 for i in stemmed_tokens_list[:10]:
-	for j in i:
-		print(j,end=" ")
-	print(" ")
+    for j in i:
+        print(j, end=" ")
+    print(" ")
 
+# 5. Check most frequent words - candidates to add to the stopword list
+listofall = [item for elem in stemmed_tokens_list for item in elem]
 
-
-#5. Check most frequent words - candidates to add to the stopword list
-listofall = [ item for elem in stemmed_tokens_list for item in elem]
-
-freq = FreqDist(listofall);
-wnum=freq.B();
-print("\nMost common words (total %d)"%wnum)
+freq = FreqDist(listofall)
+wnum = freq.B()
+print("\nMost common words (total %d)" % wnum)
 print(freq.most_common(100))
 
-
-#6. Present as tf-idf
-cleaned_documents = [ ' '.join(i) for i in stemmed_tokens_list]
+# 6. Present as tf-idf
+cleaned_documents = [' '.join(i) for i in stemmed_tokens_list]
 
 tfidf_vectorizer = TfidfVectorizer(smooth_idf=False)
-#only tf part:
-#tfidf_vectorizer = TfidfVectorizer(use_idf=False)
+# only tf part:
+# tfidf_vectorizer = TfidfVectorizer(use_idf=False)
 
 tfidf_vectorizer.fit(cleaned_documents)
 tf_idf_vectors = tfidf_vectorizer.transform(cleaned_documents)
 
 print("\nThe tf-idf values of the first document\n");
 feature_names = tfidf_vectorizer.get_feature_names_out()
-feature_index = tf_idf_vectors[0,:].nonzero()[1]
+feature_index = tf_idf_vectors[0, :].nonzero()[1]
 tfidf_scores = zip(feature_index, [tf_idf_vectors[0, x] for x in feature_index])
 for w, s in [(feature_names[i], s) for (i, s) in tfidf_scores]:
     print(w, s)
